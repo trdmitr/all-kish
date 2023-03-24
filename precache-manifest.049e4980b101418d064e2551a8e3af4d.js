f685035bf99ff9dfe31c0cf92f7ff0c754947a85
trdmitr <trdmitr@gmail.com>
2023-03-25 00:33:09 +0400

    Updates

---
+++ precache-manifest.049e4980b101418d064e2551a8e3af4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "853ecdb5a9a7cfc27637",
+    "url": "/static/css/main.f77247f8.chunk.css"
+  },
+  {
+    "revision": "853ecdb5a9a7cfc27637",
+    "url": "/static/js/main.853ecdb5.chunk.js"
+  },
+  {
+    "revision": "f13e2d7f7123de3ff015",
+    "url": "/static/js/1.f13e2d7f.chunk.js"
+  },
+  {
+    "revision": "6c3c0e8fecc6987685de",
+    "url": "/static/css/2.5241b114.chunk.css"
+  },
+  {
+    "revision": "6c3c0e8fecc6987685de",
+    "url": "/static/js/2.6c3c0e8f.chunk.js"
+  },
+  {
+    "revision": "471fb410c7b96d29691a",
+    "url": "/static/js/runtime~main.471fb410.js"
+  },
+  {
+    "revision": "f57c1202a8a62acee8a56622412c3e5b",
+    "url": "/static/media/jakobctt_8b806cbf869f1c45daf1949a025cd519.f57c1202.woff"
+  },
+  {
+    "revision": "7b8c4ec214e1410fed0ada91f8fa4d02",
+    "url": "/index.html"
+  }
+];
